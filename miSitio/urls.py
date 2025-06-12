af8b3817fde6d523bbfd086d7954b373896dd314
matias <elmorenoxpro128@gmail.com>
2025-06-12 01:07:45 -0300

    Modificaciones CSS

--- miSitio/urls.py
+++ miSitio/urls.py
@@ -3,5 +3,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',  include('blog.urls')),
-    path('evaluacion2/', views.evaluacion2, name='evaluacion2'),
 ]
