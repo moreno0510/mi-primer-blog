154a5334dcaca44cd6d0642e2606a41a95ee1408
matias <elmorenoxpro128@gmail.com>
2025-06-12 00:38:21 -0300

    Modificaciones CSS

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 from .models import Publicacion
+from .models import Libro
 admin.site.register(Publicacion)
+admin.site.register(Libro)
--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
+from datetime import date
 class Publicacion(models.Model):
     autor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     titulo = models.CharField(max_length=200)
@@ -12,12 +13,24 @@ class Publicacion(models.Model):
             self.save()
     def __str__(self):
             return self.titulo
+class Autor(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
 class Libro(models.Model):
     titulo = models.CharField(max_length=100)
-    autor = models.CharField(max_length=100)
+    autor =  models.CharField(max_length=100)
     fecha_publicacion = models.DateField()
     categoria = models.CharField(max_length=50)
+    portada = models.URLField()
+    sinopsis = models.TextField()
 
     def __str__(self):
         return self.titulo
+
+    def publicar(self):
+        self.fecha_publicacion = date.today()
+        self.save()
 
